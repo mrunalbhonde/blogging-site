a77efba44525a5f81b2dcc88272a9b2b04320e72
mrunalbhonde <bhondemrunal0@gmail.com>
2024-11-11 17:32:49 +0530

    prisma init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,24 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 // issue with serverless environments, we cannot globally access the env variables we put in our wrangler.toml
+// can only access using the context or c variable
+// therefore prisma client initialisation needs to happen inside each route
+// to show we are using connection pool using accelerate .$extends(withAccelerate())
+const app = new Hono<{
+  Variables: {
+
+  },
+  Bindings: {
+    
+  }
+}>()
+
+// typescript will kalso have a type error for variables/bindings inside prisma client init as ts doesnt know their types
 const prisma = new PrismaClient({
     datasourceUrl: env.DATABASE_URL,
 }).$extends(withAccelerate())
 
-const app = new Hono()
+
 
 app.route('/api/vi/user', userRouter);
 app.route('/ap/v1/account', accountRouter);
