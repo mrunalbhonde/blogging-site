f29aa2a69553c29abd0d7b704daf642fa39e4100
mrunalbhonde <bhondemrunal0@gmail.com>
2024-11-12 20:24:38 +0530

    blog routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -10,23 +10,96 @@ export const blogRouter = new Hono<{
     }
 }>();
 
-blogRouter.post('/*', async(c) => {
+blogRouter.use('/*', async(c, next) => {
+
+    await next();
 
 })
 
 blogRouter.post('/',async (c) =>{
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const blog = await prisma.blog.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            authorId: 1 //hardcoded
+        }
+    })
+
+    return c.json({
+        id: blog.id
+    })
 
 })
 
 blogRouter.put('/', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
+    const blog = await prisma.blog.update({
+        where: {
+            id: body.id
+        },
+        data: {
+            title: body.title,
+            content: body.content,
+        }
+    })
+
+    return c.json({
+        id: blog.id
+    })
 })
 
-blogRouter.get('/:id', async (c) => {
+blogRouter.get('/', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
+    try {
+        const blog = prisma.blog.findFirst({
+            where: {
+                id: body.id
+            }
+        })
+    
+        return c.json({
+            blog
+        })
+    } catch(e){
+        c.status(404);
+        return c.text("Blog does not exist")
+    }
 })
 
+//pagination
 blogRouter.get('/bulk', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    try {
+        const blogs = prisma.blog.findMany({
+            where: {
+                id: body.id
+            }
+        })
+    
+        return c.json({
+            blogs
+        })
+    } catch(e){
+        c.status(411);
+        return c.text("Error while fetching the blog post")
+    }
 
 })
 
